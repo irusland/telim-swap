52773d44e62d46b9c72bd3a7340b8d1790f63570
Ruslan Sirazhetdinov <ruslansir@icloud.com>
2022-06-21 09:33:03 +0500

    Torch serve

--- Makefile
+++ Makefile
@@ -4,11 +4,21 @@ include ${ENV_FILE}
 $(eval export $(shell sed -ne 's/ *#.*$$//; /./ s/=.*$$// p' ${ENV_FILE}))
 
 
+pre-init:
+	apt-get install zip unzip
+
+
+mac-pre-init:
+	brew install wget
+
+
 install:
 	poetry install
 
+
 start:
 	python3 main.py
+
 
 stop:
 	echo stop
--- src/neural_net_clients/neural_net.py
+++ src/neural_net_clients/neural_net.py
@@ -1,3 +1,9 @@
+import json
+import logging
+import shutil
+
+import requests
+
 from src.neural_net_clients.base_neural_net import NeuralNet
 
 
@@ -5,9 +11,29 @@ class Image:
     pass
 
 
+logger = logging.getLogger(__name__)
+
+
 class ImSwapNeuralNet(NeuralNet):
     def __init__(self):
         pass
 
     def swap_style(self, style_image: Image, content_image: Image) -> Image:
+        url = 'http://localhost:8080/predictions/dcgan_fashiongen'
+        r = requests.post(
+            url,
+            data=json.dumps({"number_of_images": 64, "input_gender": "Men",
+                             "input_category": "SHIRTS", "input_pose": "id_gridfs_1"}),
+            headers={"Content-Type": "application/json"},
+            stream=True
+        )
+        logger.info(r.status_code)
+        path = 'gen.jpg'
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+
+            with open(path, 'rb') as f:
+                return f.read()
         return content_image
